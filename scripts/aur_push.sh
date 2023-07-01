5edbc840d0c41e1feb344d74349b743d1030e749
TheWisker <TheWisker@protonmail.com>
2023-07-01 15:01:08 +0200

    Prerelease documentation

--- scripts/aur_push.sh
+++ scripts/aur_push.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+: '
+ @license AUR Update Git
+ aur_push.sh
+
+ Copyright (c) 2023, TheWisker.
+
+ This source code is licensed under the GNU license found in the
+ LICENSE file in the root directory of this source tree.
+'
+
 for folder in $aur_folder/*/; do
     folder_name=$(basename "${folder}")
     # Only run if it is a pkgbase-git aur package
